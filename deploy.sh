8f4db6d9b00116f809826153c744356df951f889
Diogo de Oliveira Bezerra <106110681+dbezerrahub@users.noreply.github.com>
2026-04-23 12:41:45 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Variáveis
-PRIVATE_KEY_PATH="/home/diogo/.ssh/DEV_DIOGO_PRIVATE_KEY.pem"
+PRIVATE_KEY_PATH="/home/diogo/.ssh/PRIVATE_KEY.pem"
 REMOTE_USER="diogo"
-REMOTE_HOST="54.88.250.35"
-REMOTE_PATH="/var/www/html/callauth-dist"
+REMOTE_HOST="ip"
+REMOTE_PATH="/var/www/html/projeto"
 LOCAL_PATH="/callauth/dist"
 SSH_PORT=2220
 
 BRANCH=$1
 
 ssh -i "$PRIVATE_KEY_PATH" -p $SSH_PORT $REMOTE_USER@$REMOTE_HOST "
-  sudo git config --global --add safe.directory /paynbox &&
-  cd /paynbox &&
+  sudo git config --global --add safe.directory /projeto &&
+  cd /projeto &&
   sudo git fetch origin &&
   sudo git checkout $BRANCH || git checkout -b $BRANCH &&
   sudo git pull origin $BRANCH &&
-  sudo mkdir -p /var/www/html/paynbox-dist/spa/ &&
-  sudo rm -rf /var/www/html/paynbox-dist/spa/ &&
-  sudo rsync -av /paynbox/dist/spa/ /var/www/html/paynbox-dist/spa/ &&
-  sudo chown -R diogo:www-data /var/www/html/paynbox-dist/
+  sudo mkdir -p /var/www/html/projeto-dist/spa/ &&
+  sudo rm -rf /var/www/html/projeto-dist/spa/ &&
+  sudo rsync -av /projeto/dist/spa/ /var/www/html/projeto-dist/spa/ &&
+  sudo chown -R diogo:www-data /var/www/html/projeto-dist/
 "
